3d94b6c0547a9fc8a533baf66eb465bd8d62e7cb
CTeixeira <camila.teixeira@gmail.com.br>
2022-10-12 18:31:01 -0300

    challange 07

--- #07/app.js
+++ #07/app.js
@@ -4,3 +4,10 @@ const card =  document.querySelector('.card')
 btnMenu.addEventListener('click', () => {
   card.classList.toggle('active')
 })
+
+const btnSearch =  document.querySelector('#btn-search')
+const searchBox =  document.querySelector('.search-box')
+
+btnSearch.addEventListener('click', () => {
+  searchBox.classList.toggle('active')
+})
