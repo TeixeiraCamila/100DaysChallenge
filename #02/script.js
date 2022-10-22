a02d4a7ea915fd82e5b0e971cdc48897774a6fd1
CTeixeira <camila.teixeira@gmail.com.br>
2022-10-22 15:13:49 -0300

    [ajuste] desafio 02

--- #02/script.js
+++
@@ -1,9 +0,0 @@
-let btn = document.querySelector("#burger");
-let divs = document.querySelectorAll(".line.no-animation");
-
-btn.addEventListener("click", () => {
-	divs.forEach((div) => {
-		div.classList.remove("no-animation");
-		div.classList.toggle("animation");
-	});
-});
